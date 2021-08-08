dcefb985dcea47b2c78b15e6d23bd2767939e468
mtmiranda <mattheus_miranda@hotmail.com>
2021-08-07 23:34:16 -0300

    Global app structure

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import Header from "./components/Header";
+
+import Table from "./components/Table";
+
+import { GlobalStyle } from "./styles/global";
+import "./styles/global";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Header />
+      <Table />
+    </>
+  );
+}
+
+export default App;
