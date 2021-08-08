f8b6f50be032369eb1a1cdbb4e558ab221a9fd25
mtmiranda <mattheus_miranda@hotmail.com>
2021-08-08 18:38:46 -0300

    resizing table and home wrapper

---
+++ frontend/src/components/TableComponent/style.js
@@ -0,0 +1,51 @@
+import styled from 'styled-components'
+
+export const Container = styled.main`
+    .react-bootstrap-table {
+        font-size: 10px;
+    }
+`
+
+export const TableWrapper = styled.div`
+    display: ${(props) => (props.isOpen ? 'block' : 'none')};
+`
+
+export const Content = styled.div`
+    max-width: 1120px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-direction: column;
+    margin: 0 auto;
+    padding: 20px 0;
+
+    .bootstrap-table-wrapper {
+        display: flex;
+        flex-direction: column;
+        align-content: center;
+        align-items: center;
+    }
+
+    .home-wrapper {
+        max-width: 500px;
+        width: 100%;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+
+        @media (max-width: 768px) {
+            padding: 20px;
+        }
+
+        & span {
+            color: var(--red);
+            font-weight: bold;
+            filter: opacity(0.8);
+        }
+
+        & p {
+            margin: 20px 0;
+        }
+    }
+`
