521124a91e68943fb82cb1d6370f1a445f7041f2
mtmiranda <mattheus_miranda@hotmail.com>
2021-08-08 18:34:27 -0300

    New styles for registries btn

--- frontend/src/components/Header/style.js
+++ frontend/src/components/Header/style.js
@@ -1,14 +1,29 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export const HeaderWrapper = styled.div`
-  background: var(--background-header);
-`;
+    background: var(--white);
+`
 
 export const Content = styled.div`
-  max-width: 1120px;
-  margin: 0 auto;
-  padding: 2rem 1rem 5rem;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-`;
+    max-width: 1120px;
+    margin: 0 auto;
+    padding: 2rem 1rem 5rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    .registries-btn {
+        font-size: 1rem;
+        color: #fff;
+        background: var(--text-title);
+        border: 0;
+        padding: 0 2rem;
+        border-radius: 0.25rem;
+        height: 3rem;
+        transition: filter 0.2s;
+
+        &:hover {
+            filter: brightness(0.9);
+        }
+    }
+`
