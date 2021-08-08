35dcb3253f1918904c87d3b4fa6128f82db808fb
mtmiranda <mattheus_miranda@hotmail.com>
2021-08-08 18:33:52 -0300

    Sending registriesButton props onclick

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,19 +1,25 @@
-import React from "react";
-import { Content, HeaderWrapper } from "./style";
+import React from 'react'
+import { Content, HeaderWrapper } from './style'
 
-import logo from "../../assets/logo.png";
+import logo from '../../assets/logo.png'
 
-function Header() {
-  return (
-    <HeaderWrapper>
-      <header>
-        <Content>
-          <img src={logo} alt="Logo laboratorio" />
-          <button type="button">Consultar registros</button>
-        </Content>
-      </header>
-    </HeaderWrapper>
-  );
+function Header({ onOpenNewTableRegistry }) {
+    return (
+        <HeaderWrapper>
+            <header>
+                <Content>
+                    <img src={logo} alt="Logo laboratorio" />
+                    <button
+                        onClick={onOpenNewTableRegistry}
+                        className="registries-btn"
+                        type="button"
+                    >
+                        Consultar registros
+                    </button>
+                </Content>
+            </header>
+        </HeaderWrapper>
+    )
 }
 
-export default Header;
+export default Header
